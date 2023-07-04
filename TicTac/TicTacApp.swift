73f6702d14448b8637f7ab2ccb6b84274fa54e6b
Farangis <farangis98@ya.ru>
2023-07-04 09:38:15 +0500

    Initial Commit

---
+++ TicTac/TicTacApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacApp.swift
+//  TicTac
+//
+//  Created by IMacIBT1 on 04/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
